2314a2e9022d081ff12a7330ed02c15f619ca5e0
PC-Ngumoha <pcngumoha@gmail.com>
2022-04-08 14:44:36 +0000

    Update C program file: 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ int main(void)
 
 	if (last_digit > 5)
 	{
-		printf("Last digit of %d is  %d and is greater than 5\n",
+		printf("Last digit of %d is %d and is greater than 5\n",
 				n, last_digit);
 	}
 	else if ((last_digit != 0)  && (last_digit < 6))
