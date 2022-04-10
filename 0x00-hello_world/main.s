442cbb59fed685fdd58e66265548c152c56e7713
PC-Ngumoha <pcngumoha@gmail.com>
2022-04-10 20:02:26 +0000

    Update Bash script: 100-intel

--- 0x00-hello_world/main.s
+++ 0x00-hello_world/main.s
@@ -1,4 +1,5 @@
 	.file	"main.c"
+	.intel_syntax noprefix
 	.text
 	.section	.rodata
 .LC0:
@@ -10,15 +11,15 @@ main:
 .LFB0:
 	.cfi_startproc
 	endbr64
-	pushq	%rbp
+	push	rbp
 	.cfi_def_cfa_offset 16
 	.cfi_offset 6, -16
-	movq	%rsp, %rbp
+	mov	rbp, rsp
 	.cfi_def_cfa_register 6
-	leaq	.LC0(%rip), %rdi
+	lea	rdi, .LC0[rip]
 	call	puts@PLT
-	movl	$0, %eax
-	popq	%rbp
+	mov	eax, 0
+	pop	rbp
 	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
