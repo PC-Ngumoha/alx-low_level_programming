28f16177f572c0975b4880a9905f00e9100a76b9
PC-Ngumoha <pcngumoha@gmail.com>
2022-04-10 18:55:49 +0000

    Update Bash script: 100-intel

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -7,6 +7,7 @@
  */
 int main(void)
 {
+	puts("Holberton School");
 	return (0);
 }
 
--- 0x00-hello_world/main.s
+++ 0x00-hello_world/main.s
@@ -1,5 +1,9 @@
 	.file	"main.c"
 	.text
+	.section	.rodata
+.LC0:
+	.string	"Holberton School"
+	.text
 	.globl	main
 	.type	main, @function
 main:
@@ -11,6 +15,8 @@ main:
 	.cfi_offset 6, -16
 	movq	%rsp, %rbp
 	.cfi_def_cfa_register 6
+	leaq	.LC0(%rip), %rdi
+	call	puts@PLT
 	movl	$0, %eax
 	popq	%rbp
 	.cfi_def_cfa 7, 8
