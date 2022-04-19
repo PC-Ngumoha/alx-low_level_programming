597c4665c80b2f41bafb5b4d6133920f22e02221
PC-Ngumoha <pcngumoha@gmail.com>
2022-04-19 08:31:04 +0000

    Task 3

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -8,5 +8,7 @@ int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 void print_times_table(int n);
+int convert_day(int, int);
+void print_remaining_days(int, int, int);
 
 #endif
