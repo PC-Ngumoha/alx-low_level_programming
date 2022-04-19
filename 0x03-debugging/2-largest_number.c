d4be7e96d0bf5b1b6d977ef0af1898d278d5ba13
PC-Ngumoha <pcngumoha@gmail.com>
2022-04-19 11:43:55 +0000

    Task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,15 +12,13 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
-	{
-		largest = a;
-	}
-	else if (b > a && a > c)
+	largest = a;
+
+	if (a < b && c < b)
 	{
 		largest = b;
 	}
-	else
+	else if (a < c && b < c)
 	{
 		largest = c;
 	}
