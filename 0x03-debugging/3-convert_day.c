ce0fd5c934c14a927d6742f3c76cfa0afee133c4
PC-Ngumoha <pcngumoha@gmail.com>
2022-04-19 08:37:03 +0000

    Task 3

--- 0x03-debugging/3-convert_day.c
+++ 0x03-debugging/3-convert_day.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * * convert_day - converts day of month to day of year, without accounting
- * * for leap year
- * * @month: month in number format
- * * @day: day of month
- * * Return: day of year
- * */
+ * convert_day - converts day of month to day of year, without accounting
+ * for leap year
+ * @month: month in number format
+ * @day: day of month
+ * Return: day of year
+ */
 
 int convert_day(int month, int day)
 {
