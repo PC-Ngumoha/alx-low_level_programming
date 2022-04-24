eb238918d1bff175b4341e2141aaa2d68b128266
PC-Ngumoha <pcngumoha@gmail.com>
2022-04-24 19:16:34 +0000

    Update C program file: 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,13 +13,18 @@ char *cap_string(char *str)
 	i = 0;
 	while (str[i] != '\0')
 	{
-		if (str[i] == 32 || str[i] == 9 || str[i] == 13
+		if (str[i] == 32 || str[i] == '\t' || str[i] == '\n'
 				|| str[i] == 44 || str[i] == 59
 				|| str[i] == 46 || str[i] == 33
 				|| str[i] == 63 || str[i] == 34
 				|| str[i] == 40 || str[i] == 41
 				|| str[i] == 123 || str[i] == 125)
 		{
+			if (str[i] == '\t')
+			{
+				str[i] = ' ';
+			}
+
 			if (str[i + 1] >= 97 && str[i + 1] <= 122)
 			{
 				str[i + 1] = str[i + 1] - 32;
