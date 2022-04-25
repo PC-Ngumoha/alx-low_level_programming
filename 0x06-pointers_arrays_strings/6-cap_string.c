d73ff6f4176155135d1d6a4f91609707bf53f944
PC-Ngumoha <pcngumoha@gmail.com>
2022-04-25 13:18:11 +0000

    Update C program file: 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,26 +11,39 @@ char *cap_string(char *str)
 	int i;
 
 	i = 0;
+	if (str[0] >= 97 && str[0] <= 122)
+	{
+		str[0] -= 32;
+	}
 	while (str[i] != '\0')
 	{
-		if (str[i] == 32 || str[i] == '\t' || str[i] == '\n'
-				|| str[i] == 44 || str[i] == 59
-				|| str[i] == 46 || str[i] == 33
-				|| str[i] == 63 || str[i] == 34
-				|| str[i] == 40 || str[i] == 41
-				|| str[i] == 123 || str[i] == 125)
+		switch (str[i])
 		{
-			if (str[i] == '\t')
-			{
-				str[i] = ' ';
-			}
-
-			if (str[i + 1] >= 97 && str[i + 1] <= 122)
-			{
-				str[i + 1] = str[i + 1] - 32;
-			}
+			case ' ':
+			case '\n':
+			case '\t':
+			case ',':
+			case '.':
+			case ';':
+			case '!':
+			case '?':
+			case '"':
+			case '(':
+			case ')':
+			case '{':
+			case '}':
+				if (str[i + 1] >= 97 && str[i + 1] <= 122)
+				{
+					str[i + 1] -= 32;
+				}
+				if (str[i] == '\t')
+				{
+					str[i] = ' ';
+				}
+				break;
+			default:
+				break;
 		}
-
 		i++;
 	}
 
