dbcc7b12eaefd8bd35a39b2d38504dbaf65c373b
PC-Ngumoha <pcngumoha@gmail.com>
2022-04-26 15:36:01 +0000

    Create C program file: 2-strchr.c . Update README.md

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -18,5 +18,12 @@ char *_strchr(char *s, char c)
 		s++;
 	}
 
-	return (s);
+	if (*s == '\0')
+	{
+		return (NULL);
+	}
+	else
+	{
+		return (s);
+	}
 }
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
+#define NULL 0
+
 int _putchar(char);
 char *_memset(char *, char, unsigned int);
 char *_memcpy(char *, char *, unsigned int);
