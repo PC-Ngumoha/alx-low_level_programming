e3f92759aa830bb9efbadea70f423e93543afbc7
PC-Ngumoha <pcngumoha@gmail.com>
2022-04-26 16:17:21 +0000

    Update C program file: 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,21 +5,18 @@
  * @s: String to search within
  * @c: Character to search for within @s
  *
- * Return: @s or @s++ (Depending on the location of c)
+ * Return: @s or NULL (Depending on whether c is found)
  */
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	i = 0;
-	while (*(s + i) != '\0')
+	while (*s != '0')
 	{
-		if (*(s + i) == c)
+		if (*s == c)
 		{
-			return (s + i);
+			return (s);
 		}
-		i++;
+		s++;
 	}
 
-	return (NULL);
+	return (0);
 }
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
-#define NULL '\0'
 
 int _putchar(char);
 char *_memset(char *, char, unsigned int);
