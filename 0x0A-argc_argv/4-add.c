79dc6938565a852244c4a9e8d0ca06a2ffd7be0f
PC-Ngumoha <pcngumoha@gmail.com>
2022-05-03 10:45:40 +0000

    Update C program file: 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
+
+int is_num(char *str);
 
 /**
  * main - main entry point for the program
@@ -16,10 +19,9 @@ int main(int argc, char *argv[])
 	sum = 0;
 	for (i = 1; i < argc; i++)
 	{
-		num = atoi(argv[i]);
-
-		if (num)
+		if (is_num(argv[i]))
 		{
+			num = atoi(argv[i]);
 			if (num > 0)
 				sum += num;
 		}
@@ -33,3 +35,26 @@ int main(int argc, char *argv[])
 
 	return (0);
 }
+
+/**
+ * is_num - determines if a string is an integer
+ * @str: string to be examined
+ *
+ * Return: 0 or 1
+ */
+int is_num(char *str)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < strlen(str))
+	{
+		if (isdigit(str[i]) == 0)
+		{
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
