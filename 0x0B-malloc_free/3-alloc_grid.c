67b98c0589e958fb31543e85feee9e4d216d8d9e
PC-Ngumoha <pcngumoha@gmail.com>
2022-05-05 14:15:03 +0000

    Update C program file: 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,13 +18,20 @@ int **alloc_grid(int width, int height)
 		/*Create the Grid*/
 		ptr = (int **) malloc(sizeof(int *) * height);
 		if (ptr == 0)
+		{
+			free(ptr);
 			return (0);
+		}
 
 		for (i = 0; i < height; i++)
 		{
 			ptr[i] = (int *) malloc(sizeof(int) * width);
 			if (ptr[i] == 0)
+			{
+				free(ptr[i]);
+				free(ptr);
 				return (0);
+			}
 		}
 
 		/*Set all grid points to zero*/
