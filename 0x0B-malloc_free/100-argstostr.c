de78d02bba4cda1bd2558f96ecb95229367bc4ac
PC-Ngumoha <pcngumoha@gmail.com>
2022-05-06 10:17:59 +0000

    Update C program file: 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,10 +1,6 @@
 #include "holberton.h"
 #include <stdlib.h>
 
-
-int get_length(char *str);
-char *_str_concat(char *s1, char *s2);
-
 /**
  * argstostr - concatenates the command line arguments into a string
  * @ac: count of command line arguments
@@ -15,95 +11,40 @@ char *_str_concat(char *s1, char *s2);
 char *argstostr(int ac, char **av)
 {
 	char *str;
-	int i;
+	int i, j, k;
+	int count;
 
-	str = "";
 	if ((ac == 0) || (av == NULL))
 	{
 		return (NULL);
 	}
-	else
+
+	count = 0;
+	for (i = 0; i < ac; i++)
 	{
-		for (i = 0; i < ac; i++)
+		for (j = 0; av[i][j] != '\0'; j++)
 		{
-			str = _str_concat(str, av[i]);
-			if (str == NULL)
-			{
-				return (NULL);
-			}
+			count++;
 		}
 	}
-	return (str);
-}
-
-
-/**
- * _str_concat - concatenates two strings
- * @s1: first string parameter
- * @s2: second string parameter
- *
- * Return: concatenation of @s1 and @s2
- */
-char *_str_concat(char *s1, char *s2)
-{
-	int size1, size2, total_size;
-	int i, j;
-	char *result;
-
-	size1 = get_length(s1);
-	size2 = get_length(s2);
-	total_size = size1 + size2;
-	result = (char *) malloc((sizeof(char) * total_size) + 1);
-	if (result == NULL)
+	count = count + ac + 1;
+	str = (char *) malloc(sizeof(char) * count);
+	if (str == NULL)
 	{
 		return (NULL);
 	}
-	i = 0;
-	j = 0;
-	while (i < total_size)
+
+	k = 0;
+	for (i = 0; i < ac; i++)
 	{
-		if (i < size1)
+		for (j = 0; av[i][j] != '\0'; j++)
 		{
-			result[i] = s1[i];
+			str[k] = av[i][j];
+			k++;
 		}
-		else if (i >= size1)
-		{
-			result[i] = s2[j];
-			j++;
-		}
-		i++;
-	}
-	result[total_size] = '\n';
-	result[total_size + 1] = '\0';
-
-	return (result);
-}
-
-
-
-
-/**
- * get_length - returns the length of a string
- * @str: String parameter
- *
- * Return: length of @str
- */
-int get_length(char *str)
-{
-	int length, i;
-
-	if (str == NULL)
-	{
-		return (0);
+		str[k] = '\n';
+		k++;
 	}
 
-	length = 0;
-	i = 0;
-	while (str[i] != '\0')
-	{
-		length++;
-		i++;
-	}
-
-	return (length);
+	return (str);
 }
