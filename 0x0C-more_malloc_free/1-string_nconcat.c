3f715dcae1282218ce2f33450684a1f9d48ad97e
PC-Ngumoha <pcngumoha@gmail.com>
2022-05-09 14:55:09 +0000

    Create C program file: 1-string_nconcat.c Update README.md

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
 
-unsigned int get_size(char *str);
 
 /**
  * string_nconcat - concatenates s1 to n elements of s2
@@ -17,51 +16,37 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, j;
 	char *str;
 
-	size1 = get_size(s1);
-	size2 = get_size(s2);
-	if (n < size2)
-	{
-		size2 = n;
-	}
-	total_size = size1 + size2;
-	str = malloc((sizeof(char) * total_size) + 1);
+	size1 = 0, size2 = 0;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[size1] != '\0')
+		size1++;
+
+	while (s2[size2] != '\0')
+		size2++;
+
+	if (n > size2)
+		n = size2;
+
+	total_size = size1 + n + 1;
+	str = malloc(sizeof(char) * total_size);
+	if (str == NULL)
+		return (0);
+
 	i = 0;
 	for (j = 0; j < size1; j++)
 	{
 		str[i] = s1[j];
 		i++;
 	}
-	for (j = 0; j < size2; j++)
+	for (j = 0; j < n; j++)
 	{
 		str[i] = s2[j];
 		i++;
 	}
 	str[i] = '\0';
 	return (str);
-}
-
-
-/**
- * get_size - returns number of characters in str
- * @str: string parameter
- *
- * Return: size of @str
- */
-unsigned int get_size(char *str)
-{
-	unsigned int i, size;
-
-	if (str == NULL)
-	{
-		return (0);
-	}
-
-	i = 0;
-	size = 0;
-	while (str[i] != '\0')
-	{
-		size++;
-		i++;
-	}
-	return (size);
 }
