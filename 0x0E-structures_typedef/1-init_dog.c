5bc7d11772b7f0307cfb905644ceff33c7504d80
PC-Ngumoha <pcngumoha@gmail.com>
2022-05-11 14:10:24 +0000

    Create C program file: 1-init_dog . Update README.md

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  * init_dog - initializes the details of a dog
@@ -11,8 +12,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		return;
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
-	return;
 }
