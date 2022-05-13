bc5fc8d3f8098d5a231a1905d25b10a5727cf221
PC-Ngumoha <pcngumoha@gmail.com>
2022-05-13 17:21:02 +0000

    Create C program file: 1-array_iterator.c . Update README.md

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,7 +13,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (action == NULL)
+	if (action == NULL || array == NULL)
 		return;
 
 	for (i = 0; i < size; i++)
