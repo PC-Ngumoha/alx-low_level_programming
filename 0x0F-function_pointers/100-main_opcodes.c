e702b856cadc616fb337faedbf66c6012be33f35
PC-Ngumoha <pcngumoha@gmail.com>
2022-05-15 02:03:39 +0000

    Update C program file: 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -28,10 +28,14 @@ int main(int argc, char *argv[])
 	}
 	arr = (char *)main;
 
-	for (i = 0; i < (bytes - 1); i++)
+	for (i = 0; i < bytes; i++)
 	{
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
 		printf("%02hhx ", arr[i]);
 	}
-	printf("%02hhx\n", arr[i]);
 	return(0);
 }
