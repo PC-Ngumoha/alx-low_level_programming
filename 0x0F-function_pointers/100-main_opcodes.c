c5230f182192bccd7b12d16e3e3ac733a088bd18
PC-Ngumoha <pcngumoha@gmail.com>
2022-05-15 01:59:30 +0000

    Create C program file: 100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main entry point into the program
+ * @argc: number of command line arguments
+ * @argv: vector containing command line arguments
+ *
+ * Return: Always 0 (Successful)
+ */
+int main(int argc, char *argv[])
+{
+	int bytes;
+	char *arr;
+	int i;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	arr = (char *)main;
+
+	for (i = 0; i < (bytes - 1); i++)
+	{
+		printf("%02hhx ", arr[i]);
+	}
+	printf("%02hhx\n", arr[i]);
+	return(0);
+}
