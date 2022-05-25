450a693af5ae1c91918d5602d2314e140eee39c6
PC-Ngumoha <pcngumoha@gmail.com>
2022-05-25 02:28:31 +0000

    Create 100-first.c ; Update README.md, lists.h

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,5 +7,6 @@
  */
 void print_first(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
 }
