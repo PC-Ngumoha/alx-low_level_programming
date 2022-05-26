13dd6ade74526ebc41ab9baa297bb6f643dc8268
PC-Ngumoha <pcngumoha@gmail.com>
2022-05-26 13:27:18 +0000

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,6 +12,7 @@ void free_listint2(listint_t **head)
 
 	if (*head == NULL)
 	{
+		free(head);
 		head = NULL;
 		return;
 	}
@@ -22,5 +23,6 @@ void free_listint2(listint_t **head)
 		*head = (*head)->next;
 		free(temp);
 	}
+	free(head);
 	head = NULL;
 }
