c02fcbaf969c6dbf37a6ba2a2287f3903e95838b
PC-Ngumoha <pcngumoha@gmail.com>
2022-05-27 08:35:09 +0000

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,6 +15,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *curr = *head;
 	unsigned int i = 0;
 
+	if (head == NULL)
+		return (NULL);
 	if (*head == NULL)
 		return (NULL);
 
