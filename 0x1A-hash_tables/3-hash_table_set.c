ba44e8893c56553fabddba952bc4df9aeb31ba52
PC-Ngumoha <pcngumoha@gmail.com>
2022-07-29 23:52:43 +0000

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,6 +17,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	if (key == NULL || value == NULL)
 		return (0);
+	if (ht->array == NULL)
+		return (0);
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 		return (0);
