6b2a9b7784237b283c17d8c8133a2cde3857f386
PC-Ngumoha <pcngumoha@gmail.com>
2023-07-15 01:47:29 +0000

    Revisit: Complete '101-quote.c' code file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * main - main function
+ *
+ * Return: 1 on completion
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+	return (1);
+}
